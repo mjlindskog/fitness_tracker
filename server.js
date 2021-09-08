8d253016f68280111192ba790952f64396998175
mjlindskog <lindskogmarkj@gmail.com>
2021-09-07 22:29:28 -0400

    added dotenv, server running

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(morgan("dev"));
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+// routes
+// app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}!`);
+});
